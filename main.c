7321c8d21f525e8983e388448bfcb9a95129bf36
Ruttapon-sup <156408502+Ruttapon-sup@users.noreply.github.com>
2024-03-01 10:09:48 +0000

    week8lab

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ for ( i = 1; i < argc; ++i ) {
    inOrder( b.root );
 
    // traverse the tree postOrder
-   printf("\npostnOrder\n" );   postOrder( b.root );
+   printf("\npostOrder\n" );   postOrder( b.root );
 
     printf("\n");
    rotateOrder( b.root,0 );
